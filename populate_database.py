16568fc1aae2daf3730f18f6f93ede012c40bc2e
bayesmaxx <svenssonsamuel@outlook.com>
2023-09-30 10:51:41 +0200

    starting to add the forecast points to the db

--- populate_database.py
+++ populate_database.py
@@ -4,23 +4,23 @@ import pandas as pd
 import math
 from datetime import datetime
 from db_operations import add_forecast
+from db_operations import get_forecast_question
+from db_operations import update_forecast
 
+# Read the xlsx file
 xls = 'preds.xlsx'
 raw = pd.read_excel(xls, 'Open Full')
 
-raw.head()
-
+# Transpose and reset the index
 t_df = raw.T
 t_df.columns = t_df.iloc[0]
 df = t_df[1:]
-df.head()
 df.reset_index(inplace=True)
-df.iloc[:, 0]
-short_question = 
-t_df[1, 1] 
-t_df[1:2]
+
+# Since I haven't used resolution criteria up until now, I'll just set these to empty
 resolution_criteria = ""
 
+# Loop over all the forecast questions to get the parameters to add to the database
 for i in range(0, len(df)):
     short_question = df.iloc[i, 0]
     question = df.iloc[i, 1]
@@ -29,3 +29,12 @@ for i in range(0, len(df)):
     add_forecast(question, short_question, 
                  category, creation_date, resolution_criteria)
     print(i)
+
+upper_ci = ""
+lower_ci = ""
+# Loop over all the forecast points to get the parameters needed to add to the database. 
+for i in range(0, len(df)):
+    forecast_id = i + 1
+    for a in range('from prediction to predictions10'):
+        point_forecast = df.iloc[i, a]
+        
