332ff85a6a7862356a8514caa5a4dc44b88d10d4
bayesmaxx <svenssonsamuel@outlook.com>
2023-11-01 16:17:59 +0100

    added the api routing

--- forecast_app/forecaster/urls.py
+++ forecast_app/forecaster/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'forecasts', views.ForecastsViewSet)
+router.register(r'forecast_points', views.ForecastPointsViewSet)
+router.register(r'resolutions', views.ResolutionsViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('forecast/', views.forecast, name='forecast'), 
-    path('all_forecasts/', views.all_forecasts, name='all_forecasts'), 
+    path('api/', include(router.urls)), 
 ]
